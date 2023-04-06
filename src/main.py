6ad12acdba0999cdbad1fdbec0b9253a2db995c8
weoses <117029676+weoses@users.noreply.github.com>
2023-04-07 00:17:26 +0300

    img2img

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+from io import StringIO
 import threading
 from modules import shared, devices, script_callbacks, processing, masking, images
 from   src.telegram_bot import SdTgBot
 import logging
 import gradio as gr
+import yaml
 
-bo = None
+bot_instance = None
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(level=logging.DEBUG)
 
@@ -12,7 +14,7 @@ sout_h = logging.StreamHandler()
 sout_h.setLevel(level=logging.DEBUG)
 LOGGER.addHandler(sout_h)
 
-CONSTANT = {
+DEFAULT = {
     'telegram_bot_invalid_prompt_msg' : "Invalid prompt",
     'telegram_bot_generated_msg' : 'Generated!',
     'telegram_bot_generated_error_msg' : 'Error generating :(',
@@ -23,39 +25,52 @@ CONSTANT = {
                 'Generating, please wait \n'
                 'Current progress {progress} \n'
                 'ETA {eta}',    
+
+    'telegram_bot_img2img_default_prompt': 'anime',
+    'telegram_bot_commands': ["start", "help", "text2img", "img2img"],
+    'telegram_bot_steps' : 35, 
+
+
+    'telegram_bot_img2img_cmd': "img2img",
+    'telegram_bot_text2img_cmd': "text2img",
 }
 
+
+overrides_msgs_obj = None
+overrides_cmds_obj = None
+
 def create_bot():
-    global bo
+    global bot_instance
     try:
+        update_overrides()
         token = shared.opts.data.get("telegram_bot_token")
         LOGGER.debug(f'Creating telegram bot')
 
         if token:
-            bo = SdTgBot(token=token)
+            bot_instance = SdTgBot(token=token)
         else :
-            bo = None
+            bot_instance = None
     except Exception as e:
         LOGGER.exception("Cant create telegram bot %s", e)        
 
 def stop_bot():
     try:
-        global bo
-        if bo:
+        global bot_instance
+        if bot_instance:
             LOGGER.debug(f'Stop telegram bot')
-            bo.stop()
+            bot_instance.stop()
     except Exception as e:
         LOGGER.exception("Cant stop telegram bot %s", e)
 
 def start_bot():
     try:
-        global bo
-        if bo:
+        global bot_instance
+        if bot_instance:
             LOGGER.info(f'Start telegram bot')
 
             def safe_start():
                 try:
-                    bo.start()
+                    bot_instance.start()
                 except Exception as e:
                     LOGGER.exception("Cant start telegram bot:  %s", e)       
                      
@@ -66,7 +81,28 @@ def start_bot():
     except Exception as e:
         LOGGER.exception("Cant start telegram bot:  %s", e)
 
+def update_overrides():
+    global overrides_msgs_obj
+    global overrides_cmds_obj
+    overrides_msgs_obj = None
+    overrides_cmds_obj = None
+
+    try :
+        overrides_str = shared.opts.data.get('telegram_bot_msgs')
+        if overrides_str:
+            overrides_msgs_obj = yaml.load(StringIO(overrides_str), Loader=yaml.SafeLoader)
+    except Exception as e:
+        LOGGER.exception("Cant load overrides for messages: %s", e)
+    
+    try :
+        overrides_str = shared.opts.data.get('telegram_bot_cmds')
+        if overrides_str:
+            overrides_cmds_obj = yaml.load(StringIO(overrides_str), Loader=yaml.SafeLoader)
+    except Exception as e:
+        LOGGER.exception("Cant load overrides for commands: %s", e)
+
 def on_change_settings():
+    '''Change settings callback. Restart bot, reload messanges overrides'''
     try:
         stop_bot()
         create_bot()
@@ -75,6 +111,8 @@ def on_change_settings():
         LOGGER.exception("Cant restart telegram bot %s", e)
 
 def on_ui_settings():
+    """ Ui create function """
+
     from src import main
     section = ('telegram_bot', "TelegramBot")
 
@@ -95,6 +133,21 @@ def on_ui_settings():
     shared.opts.add_option("telegram_bot_negative_prompt", 
                            shared.OptionInfo('bad_person', 
                                              "Negative prompt", 
+                                             gr.Text, 
+                                             section=section))
+    
+    shared.opts.add_option("telegram_bot_commands", 
+                           shared.OptionInfo(
+                                            ["start", "help", "text2img", "img2img"], 
+                                            "Bot commands", 
+                                            gr.CheckboxGroup, 
+                                            lambda: {"choices": ["start", "help", "text2img", "img2img"]},
+                                            section=section,
+                                            onchange=main.on_change_settings)),
+    
+    shared.opts.add_option("telegram_bot_img2img_default_prompt", 
+                           shared.OptionInfo('anime', 
+                                             "Img2img default positive prompt", 
                                              gr.Text, 
                                              section=section))
     
@@ -126,9 +179,22 @@ def on_ui_settings():
                                              component_args={'step':1, 'maximum':2000}, 
                                              section=section))
     
-    shared.opts.add_option("telegram_bot_text2img_cmd", 
-                           shared.OptionInfo('txt2img', 
-                                             "Slash command for txt2img", 
+    shared.opts.add_option("telegram_bot_img2img_denoising", 
+                           shared.OptionInfo(0.68, 
+                                             "Image2image denoising strenght", 
+                                             gr.Slider,
+                                             component_args={'maximum':1}, 
+                                             section=section))
+    
+    shared.opts.add_option("telegram_bot_cmds", 
+                           shared.OptionInfo('', 
+                                             "Slash command names overrides (yml, key-value)", 
+                                             section=section,
+                                             onchange=main.on_change_settings))
+    
+    shared.opts.add_option("telegram_bot_msgs", 
+                           shared.OptionInfo('', 
+                                             "Bot messanges overrides (yml, key-value)", 
                                              section=section,
                                              onchange=main.on_change_settings))
 
@@ -141,15 +207,40 @@ def load():
     script_callbacks.on_app_started(on_app_started)
 
 def get_conf(code:str):
-    if code in CONSTANT:
-        return CONSTANT[code]
+    data = shared.opts.data.get(code)
+    if data is None:
+        return DEFAULT[code]
     
-    return shared.opts.data.get(code)
+    return data
 
 def get_msg(code:str, **kwargs):
-    strs = get_conf(code)
+    """ Get message by code, check for overrides, format by kwargs"""
+    global overrides_msgs_obj
+    strs = None
+    if overrides_msgs_obj:        
+        if code in overrides_msgs_obj:
+            strs = overrides_msgs_obj[code]
+
+    if not strs:
+        if code in DEFAULT:
+            strs = DEFAULT[code]
 
     if strs:
         return str.format(strs, **kwargs)
     
     return ''
+
+def get_cmd(code:str):
+    """ Get cmd alias by code, check for overrides"""
+    global overrides_cmds_obj
+    strs = None
+    if overrides_cmds_obj:        
+        if code in overrides_cmds_obj:
+            strs = overrides_cmds_obj[code]
+
+    if not strs:
+        if code in DEFAULT:
+            strs = DEFAULT[code]
+
+    return strs
+
--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -9,6 +9,8 @@ import pathlib
 from modules import call_queue, shared, sd_samplers
 from modules.processing import StableDiffusionProcessing, Processed, StableDiffusionProcessingTxt2Img, \
     StableDiffusionProcessingImg2Img, process_images
+from modules import txt2img
+from PIL import Image
 from src import main, utils
 
 LOGGER = logging.getLogger(__name__)
@@ -22,7 +24,7 @@ opts = shared.opts
 waiting_image_id = None
 
 class SdTgBot:
-    __slots__ = ('bot')
+    __slots__ = ('bot', 'running')
 
     def __send_waiting(self, incoming:types.Message):
         global waiting_image_id
@@ -64,18 +66,19 @@ class SdTgBot:
     def __error_waiting(self, waiting:types.Message):
         self.bot.edit_message_media(
             message_id=waiting.message_id,
-            chat_id=waiting.chat.id)
+            chat_id=waiting.chat.id,
+            media=None)
 
         self.bot.edit_message_text(message_id=waiting.message_id, 
                                    chat_id=waiting.chat.id, 
                                    text=main.get_msg('telegram_bot_generated_error_msg'))
         
-    def __gen_text2img(self, 
-                       p:StableDiffusionProcessingTxt2Img,
+    def __gen_processing(self, 
+                       p:StableDiffusionProcessing,
                        waiting_update
                        ) -> Processed:
 
-        LOGGER.debug(f'Gen text2img {p.prompt}, {p.width}x{p.height}')
+        LOGGER.debug(f'Gen {p.__class__} {p.prompt}, {p.width}x{p.height}')
 
         processed:Processed = None
 
@@ -96,8 +99,26 @@ class SdTgBot:
             eta = utils.get_eta()
             waiting_update(eta)
 
-        return processed
+        return processed     
+
+    def __get_arg_img_id(self, message:types.Message):
+        img = None
+        if message.photo: 
+            img = message.photo[-1].file_id
+
+        if not img and message.document:
+            img = message.document.file_id
         
+        if not img and message.reply_to_message:
+            msg_old = message.reply_to_message
+            
+            if msg_old.photo:
+                img = msg_old.photo[-1].file_id
+
+            if not img and msg_old.document:
+                img = message.document.file_id
+        return img
+
     def filter_msgs(self, msg:types.Message):
         auth_chats = main.get_conf('telegram_bot_autorized_chats')
         if 'ALL' == auth_chats: return True
@@ -110,7 +131,91 @@ class SdTgBot:
             chat_id=message.chat.id,
             reply_to_message_id=message.id,
             text="Hello!")
+
+    def on_img2img(self, message:types.Message):
+        prompt = utils.get_arg(message.text)
+        if not prompt:
+            prompt = main.get_conf("telegram_bot_img2img_default_prompt")
+            
+
+        img = self.__get_arg_img_id(message)
+
+        if not img:
+            self.bot.send_message(message.chat.id, main.get_msg('telegram_bot_invalid_prompt_msg'))
+            return
         
+        file_props = self.bot.get_file(img)
+        data = self.bot.download_file(file_props.file_path)
+
+        input_data = io.BytesIO(data)
+
+        img_pil = Image.open(input_data)
+
+        LOGGER.debug(f"img2img incoming {img_pil.size[0]}x{img_pil.size[1]}")        
+        # save img 
+        # ex aspect radio 1024/512 = 0.5
+        # need 128 x 128
+        # x = 256
+        # y = 128
+        # y / aspect = x
+        # 128 / 0.5 = 256
+        xy = img_pil.size[0] / img_pil.size[1]
+        if xy >= 1 : 
+            # width  > height
+            # incoming - 1000 x 500
+            # need - 512x512
+            # xy = 2
+            # calc - 512 x 256  y = (x / xy)
+            need_sizes = (int(main.get_conf('telegram_bot_img_width')), int(main.get_conf('telegram_bot_img_width') / xy))
+        else :
+            # width < height
+            # incoming - 500 x 1000
+            # need - 512x512
+            # xy = 0.5
+            # calc - 256 x 512 (y * xy)
+            need_sizes = (int(main.get_conf('telegram_bot_img_height') * xy)), int(main.get_conf('telegram_bot_img_height'))
+
+        img_pil = img_pil.resize(need_sizes)
+        LOGGER.debug(f"img2img resizied {img_pil.size[0]}x{img_pil.size[1]}")
+        img_pil.convert("RGB")
+
+        def generate_call():
+            p = StableDiffusionProcessingImg2Img(
+                init_images=[img_pil],
+                outpath_samples=opts.outdir_samples or opts.outdir_txt2img_samples,
+                outpath_grids=opts.outdir_grids or opts.outdir_txt2img_grids,
+                denoising_strength=main.get_conf("telegram_bot_img2img_denoising"),
+                resize_mode=2,
+                prompt=prompt,
+                negative_prompt=main.get_conf('telegram_bot_negative_prompt'),
+                sampler_name=sd_samplers.samplers[0].name,
+                steps=int(main.get_conf('telegram_bot_steps')),
+                cfg_scale=main.get_conf('telegram_bot_cfg_scale'),
+                width=int(main.get_conf('telegram_bot_img_width')),
+                height=int(main.get_conf('telegram_bot_img_height'))
+            )
+            waiting = self.__send_waiting(incoming=message)
+
+            res = self.__gen_processing(
+                p,
+                lambda x: self.__update_waiting(waiting, x[0], x[1]))
+            
+            if not res:
+                self.__error_waiting(message)
+                return
+            
+            if len(res.images) == 0:
+                self.__error_waiting(message)
+                return
+            
+            img = res.images[0]
+            output_data = io.BytesIO()
+            img.save(output_data, format='jpeg')
+            output_data.seek(0)
+            self.__finish_waiting(waiting, output_data)
+
+        call_queue.wrap_queued_call(generate_call)()
+           
     def on_txt2img(self, message:types.Message):
         prompt = utils.get_arg(message.text)
         if not prompt:
@@ -129,11 +234,12 @@ class SdTgBot:
                 steps=int(main.get_conf('telegram_bot_steps')),
                 cfg_scale=main.get_conf('telegram_bot_cfg_scale'),
                 width=int(main.get_conf('telegram_bot_img_width')),
-                height=int(main.get_conf('telegram_bot_img_width')),
+                height=int(main.get_conf('telegram_bot_img_height')),
             )
+            
             waiting = self.__send_waiting(incoming=message)
 
-            res = self.__gen_text2img(
+            res = self.__gen_processing(
                 p,
                 lambda x: self.__update_waiting(waiting, x[0], x[1]))
             
@@ -154,25 +260,50 @@ class SdTgBot:
         call_queue.wrap_queued_call(generate_call)()
 
     def init_msgs(self):
-        self.bot.register_message_handler(
-            callback=self.on_cmd_start,
-            func=self.filter_msgs,
-            commands=['start'])
+        modes = main.get_conf('telegram_bot_commands')
+
+        if 'start' in modes:
+            self.bot.register_message_handler(
+                callback=self.on_cmd_start,
+                func=self.filter_msgs,
+                commands=['start'])
         
-        self.bot.register_message_handler(
-            callback=self.on_txt2img,
-            func=self.filter_msgs,
-            commands=['txt2img'])
-        
+
+        if 'text2img' in modes:
+            self.bot.register_message_handler(
+                callback=self.on_txt2img,
+                func=self.filter_msgs,
+                commands=[main.get_cmd("telegram_bot_text2img_cmd")])
+
+        if 'img2img' in modes:    
+            img2img_filter = '/'+main.get_cmd("telegram_bot_img2img_cmd")
+            self.bot.register_message_handler(
+                callback=self.on_img2img,
+                func=lambda x : self.filter_msgs(x) 
+                                and x.text 
+                                and x.text.startswith(img2img_filter) )
     
     def start(self):
-        self.bot.polling(non_stop=True)
+        
+        while True:
+            time.sleep(3) # TODO wait for polling stops
+            try:
+                self.running = True
+                self.bot.polling()
+            except Exception as e:
+                LOGGER.warning("Telegram polling error - %s", e)
+    
+            if not self.running:
+                return
+
 
     def stop(self):
+        self.running = False
         self.bot.stop_polling()
 
     def __init__(self, token:str) -> None:
         self.bot = TeleBot(token=token)
+        self.running = False
         self.init_msgs()
 
 
